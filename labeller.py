992780b4e7ba29b48d37e2bd983a5da9f7308a55
afrozalm <alam.afroz011@gmail.com>
2017-09-01 20:21:34 +0530

    working labeller

--- labeller.py
+++ labeller.py
@@ -2,70 +2,63 @@ from PIL import Image
 from itertools import product
 import numpy as np
 import os
-
-
-def same_side(pt, prev_pt, mid_pt, next_pt):
-
-    return True
+from matplotlib import pyplot as plt
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 
 def lies_in(bound, x, y):
-    prev_pt = map(int, bound[0].split(','))
-    mid_pt = map(int, bound[1].split(','))
-    for next_pt in bound[2:]:
-        next_pt = map(int, next_pt.split(','))
-        if not same_side([x, y], prev_pt, mid_pt, next_pt):
-            return False
-        prev_pt = mid_pt
-        mid_pt = next_pt
+    polygon = map(lambda x: map(int, x.split(',')), bound)
+    polygon = Polygon(polygon)
+    point = Point(x, y)
 
-    return True
+    return polygon.contains(point)
 
 
 def get_label(x, y, lines):
-    face_bound = lines[1:41]    # 1
-    nose_bound = lines[41:58]   # 2
-    lips_out_u = lines[58:72]   # 3
-    lips_out_l = lines[72:86]   # 4
-    lips_in_u = lines[86:100]   # 5
-    lips_in_l = lines[100:114]  # 6
-    eye_right_u = lines[114:124]  # 7
-    eye_right_l = lines[124:134]  # 8
-    eye_left_u = lines[134:144]   # 9
-    eye_left_l = lines[144:154]   # 10
-    eyebr_right_u = lines[154:164]  # 11
-    eyebr_right_l = lines[164:174]  # 12
-    eyebr_left_u = lines[174:184]   # 13
-    eyebr_left_l = lines[184:194]   # 14
+    # face_bound = lines[1:41]
+    nose_bound = lines[42:59]   # 1
 
-    if lies_in(face_bound, x, y):
+    lips_out_u = lines[59:73]
+    lips_in_u = lines[87:101]
+    lips_upper = lips_out_u + lips_in_u  # 2
+
+    lips_in_l = lines[101:115]
+    lips_out_l = lines[73:87]
+    lips_lower = lips_in_l + lips_out_l  # 3
+
+    eye_right_u = lines[115:125]
+    eye_right_l = lines[125:135]
+    eye_right = eye_right_u + eye_right_l  # 4
+
+    eye_left_u = lines[135:145]
+    eye_left_l = lines[145:155]
+    eye_left = eye_left_u + eye_left_l  # 5
+
+    eyebr_right_u = lines[155:165]
+    eyebr_right_l = lines[165:175]
+    eyebr_right = eyebr_right_u + eyebr_right_l  # 6
+
+    eyebr_left_u = lines[175:185]
+    eyebr_left_l = lines[185:195]
+    eyebr_left = eyebr_left_u + eyebr_left_l  # 7
+
+    # if lies_in(face_bound, x, y):
+    #     return 1
+    if lies_in(nose_bound, x, y):
         return 1
-    elif lies_in(nose_bound, x, y):
+    elif lies_in(lips_upper, x, y):
         return 2
-    elif lies_in(lips_out_u, x, y):
+    elif lies_in(lips_lower, x, y):
         return 3
-    elif lies_in(lips_out_l, x, y):
+    elif lies_in(eye_right, x, y):
         return 4
-    elif lies_in(lips_in_u, x, y):
+    elif lies_in(eye_left, x, y):
         return 5
-    elif lies_in(lips_in_l, x, y):
+    elif lies_in(eyebr_right, x, y):
         return 6
-    elif lies_in(eye_right_u, x, y):
+    elif lies_in(eyebr_left, x, y):
         return 7
-    elif lies_in(eye_right_l, x, y):
-        return 8
-    elif lies_in(eye_left_u, x, y):
-        return 9
-    elif lies_in(eye_left_l, x, y):
-        return 10
-    elif lies_in(eyebr_right_u, x, y):
-        return 11
-    elif lies_in(eyebr_right_l, x, y):
-        return 12
-    elif lies_in(eyebr_left_u, x, y):
-        return 13
-    elif lies_in(eyebr_left_l, x, y):
-        return 14
     else:
         return 0
 
@@ -77,14 +70,14 @@ def make_annot_tensors(lines):
     img.load()
     img = np.asarray(img)
     h, w, _ = img.shape
-    # mask = np.zeros([h, w])
     label = np.zeros([h, w])
     # do a x, y traversal and check if point lies in any of the regions
     for x, y in product(xrange(h), xrange(w)):
-        label[x][y] = get_label(x, y, lines)
+        label[x][y] = get_label(y, x, lines)
+    return label
 
 
-DST = '../annotation/labeled-tensors'
+DST = '../annotation/labeled-tensors/'
 SRC = '../annotation'
 
 files = []
@@ -92,7 +85,13 @@ for root, dirs, f in os.walk(SRC):
     files += f
 
 for annotation_file in files:
+    outname = DST + annotation_file.split('.')[0]
+    annotation_file = os.path.join(SRC, annotation_file)
+    print annotation_file, outname
     with open(annotation_file) as f:
         lines = f.read().splitlines()
 
-    make_annot_tensors(lines)
+    label = make_annot_tensors(lines)
+    np.save(outname, label)
+    plt.imshow(label)
+    plt.imsave(outname, label)
